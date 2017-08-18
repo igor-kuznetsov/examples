9590f2efc454bcda2eddf6ce3bf52e0dc9b0df10
Igor Kuznetsov <webdevastator@gmail.com>
2017-08-18 17:46:52 +0300

    composer example

---
+++ Composer/Lib/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Examples\Composer\Lib;
+
+class App
+{
+    public function run()
+    {
+        echo 'test';
+    }
+}
--- Composer/index.php
+++ Composer/index.php
@@ -4,6 +4,7 @@ namespace Examples\Composer;
 
 require_once 'vendor/autoload.php';
 
+use Examples\Composer\Lib\App;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
@@ -11,3 +12,6 @@ $logger = new Logger('test');
 $logger->pushHandler(new StreamHandler('log.txt', Logger::WARNING));
 $logger->warning('some test warning goes here');
 $logger->error('some error goes here');
+
+$app = new App();
+$app->run();
